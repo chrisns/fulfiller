21ceac1f2ee69101064f4a67c7595fe023cf5c90
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-03-08 01:04:51 +0000

    use chai.should

--- test/helpers/sharedBootstrap.js
+++ test/helpers/sharedBootstrap.js
@@ -3,7 +3,8 @@ global.freeport = require('freeport');
 global.chai = require('chai')
   .use(require('chai-as-promised'))
   .use(require('chai-things'))
-  .use(require('sinon-chai'));
+  .use(require('sinon-chai'))
+  .should();
 global.expect = chai.expect;
 global._ = require('lodash');
 global.sinon = require('sinon');
