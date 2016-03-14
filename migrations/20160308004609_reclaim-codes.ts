111133d19c4475b03495317ca7a4ad4ff26fd2b9
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-03-14 22:58:15 +0000

    re-enable the migration

--- migrations/20160308004609_reclaim-codes.ts
+++ migrations/20160308004609_reclaim-codes.ts
@@ -1,11 +1,11 @@
-//import Knex = require("knex");
-//import Promise = require("bluebird");
-//
-//exports.up = (knex:Knex):Promise<void> =>
-//  knex.schema.createTable("reclaim", (table:Knex.TableBuilder) => {
-//    table.increments("id").primary();
-//    table.string("code");
-//  });
-//
-//exports.down = (knex:Knex):Promise<void> =>
-//  knex.schema.dropTable("users");
+import Knex = require("knex");
+import Promise = require("bluebird");
+
+exports.up = (knex:Knex):Promise<void> =>
+  knex.schema.createTable("reclaim", (table:Knex.TableBuilder) => {
+    table.increments("id").primary();
+    table.string("code");
+  });
+
+exports.down = (knex:Knex):Promise<void> =>
+  knex.schema.dropTable("users");
